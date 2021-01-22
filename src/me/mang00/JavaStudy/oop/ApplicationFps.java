5fc11a41e579eb6516feff8e5ed0e5a8aabce7eb
miniky04 <miniky04@naver.com>
2021-01-22 23:49:47 +0900

    Java object-oriented programming Application version Fps modification upload

--- src/me/mang00/JavaStudy/oop/ApplicationFps.java
+++ src/me/mang00/JavaStudy/oop/ApplicationFps.java
@@ -3,10 +3,6 @@ package me.mang00.JavaStudy.oop;
 class Total {
     int hp, damage; //전역변수 -> 초깃값이 0이다.
 
-//    public void print() {
-//        System.out.println(this);
-//    }
-
     // this 는 전역변수를 가리킨다.
     // 전역변수와 매개변수의 변수명이 같을 경우 this.hp = hp; 를 한다.
     // 만약 전역변수와 매개변수의 변수명이 다를 경우 hp1 = hp; 를 해도 무관하다.
@@ -35,10 +31,6 @@ public class ApplicationFps {
         ar.setHp(100);
         ar.setDamage(20);
         ar.totalHp();
-
-//        ar.print();
-//        // me.miniky04.oop.Total@54bedef2
-//        // @뒤는 고유주소 !
 
         Total smg = new Total();
         smg.setHp(100);
