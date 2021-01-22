3f543f279f8abf96f9a9433e116333c16f8f1d8c
miniky04 <miniky04@naver.com>
2021-01-23 01:22:06 +0900

    Java object-oriented programming Application version Fps modification upload

--- src/me/mang00/JavaStudy/oop/ApplicationFps.java
+++ src/me/mang00/JavaStudy/oop/ApplicationFps.java
@@ -8,6 +8,8 @@ class Total {
     // 만약 전역변수와 매개변수의 변수명이 다를 경우 hp1 = hp; 를 해도 무관하다.
     public void setHp(int hp) { //매개변수 int hp
         this.hp = hp;
+        //스코프에 대한 차이를 두기 위해 this 를 쓴다. -> 변수명이 같을 때
+        // a = b; 일 경우 (int b) 이고 this 를 쓰든 안쓰든 상관 없다.
     }
 
     public void setDamage(int damage) {
