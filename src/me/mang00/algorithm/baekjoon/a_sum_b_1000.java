60d0cd45e0cb36d4ded99b1abe6bc42885cdbd1f
miniky04 <miniky04@naver.com>
2021-02-15 03:47:03 +0900

    BOJ Mathmatics bronze5 a_sum_b_1000 solve upload

---
+++ src/me/mang00/algorithm/baekjoon/a_sum_b_1000.java
@@ -0,0 +1,16 @@
+package me.mang00.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class a_sum_b_1000 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+    }
+}
