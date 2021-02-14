8633f313410740863252bb3827535b540466be12
miniky04 <miniky04@naver.com>
2021-02-15 03:45:42 +0900

    Java chapter5 Array example upload

--- src/me/mang00/JavaStudy/array/ArrayEx1.java
+++ src/me/mang00/JavaStudy/array/ArrayEx1.java
@@ -8,7 +8,10 @@ public class ArrayEx1 {
 
         int[] score = {100, 95, 90, 100, 80, 85};
 
-        for (int j : score) {
+        // 향상된 for 문
+        // 기존의 for 문과 다른점 : 인덱스를 제공해주지 않는다.
+        // 주의할 점 : 배열 내부의 값을 변경할 때는 이전 방식을 사용해야한다. (향상된 for문 사용불가능)
+        for (int j : score) { // 객체 : 배열
             sum += j;
         }
         average = sum / (float)score.length;
